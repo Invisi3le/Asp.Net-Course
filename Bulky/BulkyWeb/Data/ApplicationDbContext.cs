b590ee9b28d544fbb07ecbe3225c56433ff7325d
Monika Szewczyk <monika.szewczyk120@gmail.com>
2023-05-22 22:21:39 +0200

    Seed Category Table

--- Bulky/BulkyWeb/Data/ApplicationDbContext.cs
+++ Bulky/BulkyWeb/Data/ApplicationDbContext.cs
@@ -11,5 +11,16 @@ namespace BulkyWeb.Data
         }
 
         public DbSet<Category> Category { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder= 1 },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+           
+        }
     }
 }
