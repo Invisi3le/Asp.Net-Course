7ce91d44305cf50b9214b5f884c58fa6fbcd9e7f
Monika Szewczyk <monika.szewczyk120@gmail.com>
2023-05-23 12:23:44 +0200

    create category UI

--- Bulky/BulkyWeb/Controllers/CategoryController.cs
+++ Bulky/BulkyWeb/Controllers/CategoryController.cs
@@ -16,5 +16,10 @@ namespace BulkyWeb.Controllers
             List<Category> objCategoryList = _db.Category.ToList();
             return View(objCategoryList);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
