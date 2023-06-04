af8e70bebbd0ccb99a862a7a13a2433294486a1d
Monika Szewczyk <monika.szewczyk120@gmail.com>
2023-06-04 23:17:41 +0200

    update category view

--- Bulky/BulkyWeb/Controllers/CategoryController.cs
+++ Bulky/BulkyWeb/Controllers/CategoryController.cs
@@ -43,5 +43,44 @@ namespace BulkyWeb.Controllers
             return View();
             
         }
+
+        public IActionResult Edit(int? id) 
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Category.Find(id); // Find() is a method that finds the primary key
+           // Category? categoryFromDb1 = _db.Category.FirstOrDefault(u => u.Id == id); // FirstOrDefault() is a method that finds the first record that matches the condition
+           //Category? categoryFromDb2 = _db.Category.Where(u => u.Id == id).FirstOrDefault(); // Where() is a method that finds all records that matches the condition
+
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        // POST - EDIT
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order cannot be the same as the Category Name");
+            }
+            //if (obj.Name.ToLower() == "test")
+            //{
+            //    ModelState.AddModelError("", "Invalid name of Category");
+            //}
+            if (ModelState.IsValid)
+            {
+                _db.Category.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
     }
 }
