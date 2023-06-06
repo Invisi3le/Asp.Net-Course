fd1e9eef59c086cf329bd08985d5e91aef0885c3
Monika Szewczyk <monika.szewczyk120@gmail.com>
2023-06-06 09:20:47 +0200

    add tempdata

--- Bulky/BulkyWeb/Controllers/CategoryController.cs
+++ Bulky/BulkyWeb/Controllers/CategoryController.cs
@@ -38,6 +38,7 @@ namespace BulkyWeb.Controllers
             {
                 _db.Category.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
             return View();
@@ -73,6 +74,7 @@ namespace BulkyWeb.Controllers
             {
                 _db.Category.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index");
             }
             return View();
@@ -106,6 +108,7 @@ namespace BulkyWeb.Controllers
             }
             _db.Category.Remove(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index");
 
         }
