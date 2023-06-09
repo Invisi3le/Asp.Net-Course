d9cbd8770ee739a17cbe0aac97019d45c8ae10c8
Monika Szewczyk <monika.szewczyk120@gmail.com>
2023-06-09 11:10:22 +0200

    n-tier architecture files

--- Bulky/Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky/Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using BulkyWeb.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace BulkyWeb.Data
+namespace Bulky.DataAccess.Data
 {
     public class ApplicationDbContext : DbContext
     {
